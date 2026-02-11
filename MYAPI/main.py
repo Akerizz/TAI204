c458d2de9f6dda8b11b6d75e53bd59e8be001e69
Jorge Morales <123045839@upq.edu.mx>
2026-02-11 15:45:29 -0600

    practica 2
    Practica  no 2

--- MYAPI/main.py
+++ MYAPI/main.py
@@ -1,19 +1,57 @@
 #importaciones
 from fastapi import FastAPI
 import asyncio
+from typing import Optional
+
 
 #instancias
-app = FastAPI() #preparacion de la app
+app = FastAPI(
+    title="Mi primer API",
+    description="Jorge Armando Lopez Morales",
+    version="1.0"
+) #preparacion de la app
 
+
+#TB ficticia
+usuarios=[
+    {"id":1, "nombre":"Jorge", "edad":21},
+    {"id":2, "nombre":"Maria", "edad":28},
+    {"id":3, "nombre":"Betito", "edad":30}
+   
+]
 #Endpoints
-@app.get("/") 
+@app.get("/", tags=["Inicio"]) 
 async def Bienvenidos():
-    return {"mensaje": "Bienvenidos a mi API con FastAPI"}
+    return {"mensaje": "Bienvenidos a FastAPI amor"}
 
-@app.get("/HolaMundo")
+@app.get("/HolaMundo", tags=["Asincronia"])
 async def Hola():
-    await asyncio.sleep(1) 
+    await asyncio.sleep(5) #peticion,consultaDB,Archivos
     return {
         "mensaje": "Hola Mundo FastAPI",
         "status": 200 #
     }
+
+@app.get("/v1/usuario/{id}", tags=["Parametro Oblogatorio"]) 
+async def Consultauno(id:int):
+    return {"mensaje": "Usuario encontrado",
+            "usuario":id,
+            "status": 200}
+
+#debe de tenero otro nombre porque el anrerior tambuen era get y no nececita las llaves porque es opcional
+@app.get("/v1/usuarios/", tags=["Parametro Opcional"]) 
+async def Consultatodos(id:Optional[int]=None):
+    if id is not None:
+        for usuariok in usuarios:
+            if usuariok["id"] == id:
+                return{"mensaje": "Usuario encontrado",
+                        "usuario": usuariok,
+                        "status": 200}
+        return{"mensaje": "Usuario no encontrado",
+                "status": 200}   
+    return{"mensaje":"no se proporciono id",
+            "status": 200}
+     
+
+
+#activar entorno del uvicor:  cd MYAPI Y despues python -m uvicorn main:app --reload
